8b18c0a078ee0fce130ad5dbb72f63d529a677bf
MJbae <bmj5208@gmail.com>
2020-08-12 08:21:19 +0900

    feat: init baseballGameProject(prettier, eslint 구성 완료)

---
+++ 200809_BaseballGame/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-console': 'warn',
+  },
+};

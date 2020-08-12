7ebc9bf79b3f9d0d2861aab422fbe3ed94006261
MJbae <bmj5208@gmail.com>
2020-08-12 10:54:17 +0900

    refactor: Counter 실행문 이동(App.js->Counter.js)

--- 200809_BaseballGame/src/Counter.js
+++ 200809_BaseballGame/src/Counter.js
@@ -1,3 +1,4 @@
+// TODO: 왜 즉시실행함수를 사용한 걸까?
 const Counter = (() => {
   // Symbal 사용 목적: unique value 생성
   const VALUE = Symbol();
@@ -26,5 +27,17 @@ const Counter = (() => {
     }
   };
 })();
+
+const counter = new Counter('digit-number');
+document.getElementById('minus-btn').addEventListener('click', (e) => counter.decrease());
+document.getElementById('plus-btn').addEventListener('click', (e) => counter.increase());
+document.getElementById('start-btn').addEventListener('click', (e) => {
+  // TODO: 아래 메소드는 이벤트 취소 역할이라는데 왜 사용한거지?
+  e.preventDefault();
+  // 상위요소로 본 이벤트 전파 차단
+  e.stopPropagation();
+  // TODO: game.html에서 digit 값은 어떻게 받지?
+  location.assign(`game.html?digit=${counter.value}`);
+});
 
 export default Counter;

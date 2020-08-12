f15017545550e1133f756fa00f4bb8a1b21f0334
MJbae <bmj5208@gmail.com>
2020-08-12 10:23:16 +0900

    feat: new-game button 기능 추가 1. default value: 1로 고정 2. decrease / increase 버튼 클릭에 따른 value 값 증감 출력

---
+++ 200809_BaseballGame/src/App.js
@@ -0,0 +1,5 @@
+import Counter from './Counter.js';
+
+const counter = new Counter('digit-number');
+document.getElementById('minus-btn').addEventListener('click', (e) => counter.decrease());
+document.getElementById('plus-btn').addEventListener('click', (e) => counter.increase());

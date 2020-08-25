515f7cff8585269fc9b343730bbbb2f3b267d9f2
MJbae <bmj5208@gmail.com>
2020-08-25 11:03:56 +0900

    feat: Browser/Practice 4 추가

---
+++ 200822_WebAPI_JS/Browser/Practice 4/main.js
@@ -0,0 +1,9 @@
+/**
+ * 1. UI 생성
+ *  - 화면 상 이미지, 선, 텍스트(document.documentElement size) 표시
+ *  - 우선 기준점(좌상단)에 배치
+ * 2. 이벤트 생성
+ *  - mousemove 활용, moving 감지 시 아래와 같이 작동함
+ *  - 이미지(좌표따라), 선(top, left, bottom 등 활용), 텍스트(px로 중앙에서 아래로) 움직임
+ *
+ */
